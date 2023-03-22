2f29fc0e624fef944522f845c4b7480493897431
yas-say <yasowant@live.com>
2023-03-22 17:52:36 -0400

    Unlimited Keyword Args

--- main.py
+++ main.py
@@ -1,9 +1,22 @@
 #Unlimited Positional Args
-def add(*args):
-    sum = 0
-    for n in args:
-        sum += n
-    return sum
+# def add(*args):
+#     sum = 0
+#     for n in args:
+#         sum += n
+#     return sum
+#
+#
+# print(add(1, 2, 3))
 
 
-print(add(1, 2, 3))
+
+def calc(n, **kwargs):
+    print(kwargs)
+    print(type(kwargs))
+
+    print(n)
+    print(n+kwargs["add"])
+    print(n + kwargs["mul"])
+
+
+calc(2,add=3,mul=5)
